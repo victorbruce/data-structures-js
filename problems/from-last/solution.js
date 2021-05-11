d818c46bc2da80d791074768cefb20d6c69e82cd
victorbruce <victorbruce82@gmail.com>
2021-05-11 23:17:31 +0000

    solve circular list and from last linked list problems

---
+++ problems/from-last/solution.js
@@ -0,0 +1,30 @@
+// -- Directions
+// Given a linked list and interger n, return the element n
+// spaces from the last node in the list. Do not call 'size'
+// method of the linked list. Assume that n will be less than the 
+// length of the list.
+// -- Examples
+// const list = new List();
+// list.insertLast('a');
+// list.insertLast('b');
+// list.insertLast('c');
+// list.insertLast('d');
+// fromLast(list, 2).data // 'b'
+
+function fromLast(list, n) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+  
+  while(n > 0) {
+    fast = fast.next;
+    n--;
+  }
+
+  while(fast.next) {
+    slow = slow.next;
+    fast = fast.next
+  }
+
+  return slow;
+}
+
