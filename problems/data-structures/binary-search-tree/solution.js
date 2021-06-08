3c1ecea81c3d15109e03a9d7c4c698148036cf87
victorbruce <victorbruce82@gmail.com>
2021-06-08 19:03:54 +0000

    updated BST constructor

--- problems/data-structures/binary-search-tree/solution.js
+++ problems/data-structures/binary-search-tree/solution.js
@@ -8,36 +8,122 @@
 // a 'data' argument and return the Node in the tree with the same value.
 // If the value isn't in the tree return null.
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
+
+class BST {
+  constructor(val, left=null, right=null) {
+    this.value = val;
+    this.left = left;
+    this.right = right;
   }
 
-  insert(data) {
-    if (data < this.data && this.left) {
-      this.left.insert(data);
-    } else if (data < this.data) {
-      this.left = new Node(data);
-    } else if ( data > this.data && this.right) {
-      this.right.insert(data);
-    } else if (data > this.data) {
-      this.right = new Node(data);
+  insert(value){
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
     }
-  }
+    return this;
+  };
 
-  contains(data) {
-    if (data === this.data) {
-      return this;
+  contains(value){
+    if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value)
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    } else {
+      return true;
     }
+  };
 
-    if (data < this.data && this.left) {
-      return this.left.constains(data);
-    } else if (data > this.data && this.right) {
-      this.right.contains(data);
+  remove(value, parentNode=null){
+    if (value < this.value) {
+      if (this.left !== null) {
+        this.left.remove(value, this);
+      }
+    } else if (value > this.value) {
+      if (this.right !== null) {
+        this.right.remove(value, this);
+      }
+    } else {
+      if (this.left !== null && this.right !== null) {
+        this.value = this.right.getMinValue();
+        this.right.remove(this.value, this);
+      } else if (parentNode === null) {
+        if (this.left !== null) {
+          this.value = this.left.value;
+          this.right = this.left.right;
+          this.left = this.left.left;
+        } else if (this.right !== null) {
+          this.value = this.right.value;
+          this.left = this.right.left;
+          this.right = this.right.right;
+        } else {
+          
+        }
+      } else if (parentNode.left === this) {
+        parentNode.left = this.left !== null ? this.left : this.right;
+      } else if (parentNode.right === this) {
+        parentNode.right = this.left !== null ? this.left : this.right;
+      }
     }
+  };
 
-    return null;
+  getMinValue() {
+    if (this.left === null) {
+      return this.value;
+    } else {
+      return this.left.getMinValue();
+    }
   }
 }
+
+
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.left = null;
+//     this.right = null;
+//   }
+
+//   insert(data) {
+//     if (data < this.data && this.left) {
+//       this.left.insert(data);
+//     } else if (data < this.data) {
+//       this.left = new Node(data);
+//     } else if ( data > this.data && this.right) {
+//       this.right.insert(data);
+//     } else if (data > this.data) {
+//       this.right = new Node(data);
+//     }
+//   }
+
+//   contains(data) {
+//     if (data === this.data) {
+//       return this;
+//     }
+
+//     if (data < this.data && this.left) {
+//       return this.left.constains(data);
+//     } else if (data > this.data && this.right) {
+//       this.right.contains(data);
+//     }
+
+//     return null;
+//   }
+// }
